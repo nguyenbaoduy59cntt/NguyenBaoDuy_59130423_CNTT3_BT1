9537032822b35a6d85f5c9a74f0b79fb0ab04e28
Admin <Admin@192.168.1.6>
2020-04-11 10:11:00 +0700

    SwitchCase

---
+++ SwitchCase/src/switchcase/SwitchCase.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package switchcase;
+
+/**
+ *
+ * @author Admin
+ */
+public class SwitchCase 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        short number=1;
+        switch(number)
+        {
+            case 1: System.out.println("number one"); break;
+            case 2: System.out.println("number two"); break;
+            case 3: System.out.println("number three");break;
+            default:System.out.println("number shit");
+    }
+}
+}
+
