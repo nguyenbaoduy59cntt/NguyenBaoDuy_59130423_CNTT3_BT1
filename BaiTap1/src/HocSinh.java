f82ab290f80a7d71acba4c1ad7bd78aa42ef181c
Admin <Admin@192.168.1.6>
2020-04-16 08:27:05 +0700

    BT1

--- BaiTap1/src/HocSinh.java
+++ BaiTap1/src/HocSinh.java
@@ -8,6 +8,35 @@
  *
  * @author Admin
  */
-public class HocSinh {
+public class HocSinh extends CaNhan{
+    private String lop;
+    private String nangKhieu;
     
+    public HocSinh(String lop, String nangKhieu, String hoTen, int tuoi, String diaChi, String sdt){
+        super(hoTen, tuoi, diaChi, sdt);
+        this.lop = lop;
+        this.nangKhieu = nangKhieu;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+
+    public String getNangKhieu() {
+        return nangKhieu;
+    }
+
+    public void setNangKhieu(String nangKhieu) {
+        this.nangKhieu = nangKhieu;
+    }
+    
+    @Override
+    public void HienThiTT(){
+        System.out.println("Ho va ten hs: " + hoTen + "\n" + "Tuoi: "+tuoi+"\n"+"Dia chi: "+diaChi+"\n"+ "SDT: "+sdt+"/n" + "Lop: "
+                +lop +"\n" + "Nang khieu: "+nangKhieu);
+ }
 }
